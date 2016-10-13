ce8fbff6d7d582e5403221efb815fd0da95c9a6a
nlkassad <nlkassad@hotmail.com>
2016-10-13 15:02:39 -0400

    Check File/mime Type
    Add file-type module and use to determine the content type of the
    file that is being passed.
    
    Defines function if unsupported file type is passed.

--- bin/s3-upload.js
+++ bin/s3-upload.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const fs = require('fs');
+const fileType = require('file-type');
 
 // this lets the error say it's not a valid path instead of not a string
 // run with npm run s3-upload <filename>
@@ -17,11 +18,26 @@ const readFile = (filename) => {
   });
 };
 
-const logMessage = (data) => {
-  console.log(`${filename} is ${data.length} bytes long`);
+// return default object if fileType is given and unsupported file type
+const mimeType = (data) => {
+  return Object.assign({
+    ext: 'bin',
+    mime: 'application/octet-stream',
+  }, fileType(data));
+};
+
+const parseFile = (fileBuffer) => {
+  let file = mimeType(fileBuffer);
+  file.data = fileBuffer;
+  return file;
+};
+
+const logMessage = (file) => {
+  console.log(`${filename} is ${file.data.length} bytes long and is of type ${file.mime}`);
 };
 
 readFile(filename)
+.then(parseFile)
 .then(logMessage)
 .catch(console.error)
 ;
