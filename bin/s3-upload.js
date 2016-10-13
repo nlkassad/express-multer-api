fd7d3b644a4ff7ad503e7757a926eda44d06e778
nlkassad <nlkassad@hotmail.com>
2016-10-13 13:56:26 -0400

    Change s3 Read Script to Promise
    Change the script used in the s3 read file so that it is handled
    as a promise instead of a callback.

--- bin/s3-upload.js
+++ bin/s3-upload.js
@@ -7,10 +7,22 @@ const fs = require('fs');
 // run with npm run s3-upload <filename>
 const filename = process.argv[2] || '';
 
-fs.readFile(filename, (error, data) => {
-  if (error) {
-    return console.error(error);
-  }
+const readFile = (filename) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filename, (error, data) => {
+      if (error) {
+        reject(error);
+      }
+      resolve(data);
+    });
+  });
+};
 
+const logMessage = (data) => {
   console.log(`${filename} is ${data.length} bytes long`);
-});
+};
+
+readFile(filename)
+.then(logMessage)
+.catch(console.error)
+;
