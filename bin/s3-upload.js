30cf9ffa5f8f55390c4db487382e80946f1e827a
nlkassad <nlkassad@hotmail.com>
2016-10-14 11:46:33 -0400

    Name file uploads using crypto module
    Define the wrappers for naming the file and the directory
    this takes the passed item and puts it in a dated folder
    and randomizes the name.

--- bin/s3-upload.js
+++ bin/s3-upload.js
@@ -1,15 +1,14 @@
-"use strict";
+'use strict';
 
 // this has to come before anything else
-// we are not evaluating for return value, we are pulling in the return method
 require('dotenv').config();
 
 const fs = require('fs');
+const crypto = require('crypto');
+
 const fileType = require('file-type');
 const AWS = require('aws-sdk');
 
-// this lets the error say it's not a valid path instead of not a string
-// run with npm run s3-upload <filename>
 const filename = process.argv[2] || '';
 
 const readFile = (filename) => {
@@ -18,12 +17,14 @@ const readFile = (filename) => {
       if (error) {
         reject(error);
       }
+
       resolve(data);
     });
   });
 };
 
-// return default object if fileType is given and unsupported file type
+// return a default object in the case that fileType is given an unsupported
+// filetype to read
 const mimeType = (data) => {
   return Object.assign({
     ext: 'bin',
@@ -37,7 +38,31 @@ const parseFile = (fileBuffer) => {
   return file;
 };
 
-// instance of s3 manager constructor function
+const randomHexString = (length) => {
+  return new Promise((resolve, reject) => {
+    crypto.randomBytes(length, (error, buffer) => {
+      if (error) {
+        reject(error);
+      }
+
+      resolve(buffer.toString('hex'));
+    });
+  });
+};
+
+const nameFile = (file) => {
+  return randomHexString(16)
+  .then((val) => {
+    file.name = val;
+    return file;
+  });
+};
+
+const nameDirectory = (file) => {
+  file.dir = new Date().toISOString().split('T')[0];
+  return file;
+};
+
 const s3 = new AWS.S3({
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY,
@@ -47,18 +72,18 @@ const s3 = new AWS.S3({
 
 const upload = (file) => {
   const options = {
-    // get bucket name from aws
+    // get the bucket name from your AWS S3 console
     Bucket: 'nsm-datastore-01',
-    // attach the fileBuffer as a stream to send to s3
+    // attach the fileBuffer as a stream to send to S3
     Body: file.data,
-    // allow anyone to access the url of the uploaded file
+    // allow anyone to access the URL of the uploaded file
     ACL: 'public-read',
-    // tell amazon what the mime type is
+    // tell S3 what the mime-type is
     ContentType: file.mime,
-    // pick a fileype for s3 to use for upload
-    Key: `test/test.${file.ext}`
+    // pick a filename for S3 to use for the upload
+    Key: `${file.dir}/${file.name}.${file.ext}`
   };
-  // only passing data down promise chain
+
   return new Promise((resolve, reject) => {
     s3.upload(options, (error, data) => {
       if (error) {
@@ -71,12 +96,16 @@ const upload = (file) => {
 };
 
 const logMessage = (response) => {
+  // turn the pojo into a string so I can see it on the console
   console.log(`the response from AWS was ${JSON.stringify(response)}`);
 };
 
 readFile(filename)
 .then(parseFile)
+.then(nameFile)
+.then(nameDirectory)
 .then(upload)
 .then(logMessage)
+// .then(console.log)
 .catch(console.error)
 ;
